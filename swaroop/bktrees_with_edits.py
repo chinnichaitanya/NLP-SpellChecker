341975cd7d64652ba89c24c064b39c022ce9606b
jjkotni <kjjswaroop@gmail.com>
2016-09-11 11:57:32 +0530

    BK Trees with edits

--- swaroop/bktrees_with_edits.py
+++ swaroop/bktrees_with_edits.py
@@ -9,10 +9,15 @@ Licensed under the PSF license: http://www.python.org/psf/license/
 - Adam Hupp <adam@hupp.org>
 """
 from itertools import imap, ifilter
+from collections import namedtuple
 
 import pickle
 import time
 import sys
+
+edits = namedtuple("edits", "error correction edit_Type")
+edit_list = [];
+
 class BKTree:
     def __init__(self, distfn, words):
         """
@@ -115,6 +120,71 @@ def levenshtein(s, t):
                            )
     return d[m][n]
 
+def levenshtein_edits(s, t):
+    edits = namedtuple("edits", "error correction edit_Type")
+    edit_list = [];
+    m, n = len(s), len(t)
+    d = [range(n+1)]
+    # print d
+    d += [[i] for i in range(1,m+1)]
+    # print m
+    for i in range(0,m):
+        for j in range(0,n):
+            cost = 1
+            if s[i] == t[j]: cost = 0
+
+            d[i+1].append( min(d[i][j+1]+1, # deletion
+                               d[i+1][j]+1, #insertion
+                               d[i][j]+cost) #substitution
+                           )
+   
+    # print d
+    k = len(s)
+    l = len(t)
+    while((k>0) or (l>0)):
+        if (k==0):
+          temp_min = d[k][l-1]; x_flag=1
+        elif (l==1 and k != 1):
+          temp_min = d[k-1][l]; another_flag=1
+          k = k-1
+          # print "I came here"
+          if not (edits(t[l-1],t[l-1]+s[k],"Addition") in edit_list) :
+            edit_list.append(edits(t[l-1],t[l-1]+s[k],"Addition"))
+        elif (k==1 and l != 1):
+          temp_min = d[k][l-1]; another_flag_2=1
+          l = l-1
+          if not (edits(t[l-1:l+1],t[l-1],"Removal") in edit_list) :
+                edit_list.append(edits(t[l-1:l+1],t[l-1],"Removal"))
+        elif (l==0):
+          temp_min = d[k-1][l]; y_flag=1
+        else:
+          temp_min = min(d[k-1][l],d[k][l-1],d[k-1][l-1]); x_flag=0; y_flag=0;another_flag=0;another_flag_2=0
+        if (not (x_flag or y_flag or another_flag or another_flag_2)):
+          if (temp_min == d[k-1][l-1]):
+            if(s[k-1] == t[l-1]):
+              k = k-1; l = l-1
+            else :
+              k = k-1; l = l-1
+              if not (edits(t[l],s[k],"Substitution") in edit_list) :
+                edit_list.append(edits(t[l],s[k],"Substitution"))
+          elif (temp_min == d[k][l-1]):
+            l = l-1
+            if not (edits(t[l-1:l+1],t[l-1],"Removal") in edit_list) :
+                edit_list.append(edits(t[l-1:l+1],t[l-1],"Removal"))
+          else:
+            k = k-1
+            if not (edits(t[l-1],t[l-1]+s[k],"Addition") in edit_list) :
+                edit_list.append(edits(t[l-1],t[l-1]+s[k],"Addition"))
+        else:
+          if x_flag:
+            print "x_flag was helpful!"
+            l=l-1
+            if not (edits(t[l-1:l+1],t[l-1],"Removal") in edit_list) :
+                edit_list.append(edits(t[l-1:l+1],t[l-1],"Removal"))
+          if y_flag:
+            k = k-1
+            print "y_flag was helpful!"         
+    return edit_list
 
 def dict_words(dictfile="/usr/share/dict/american-english"):
     "Return an iterator that produces words in the given dictionary."
@@ -139,8 +209,13 @@ tree =pickle.load(pkl_file)
    # pickle.dump( tree, open( "save.p", "wb" ) )
 load_finish = time.time()
 start = time.time()
-candidates = tree.query("comitte",2) 
+candidates = tree.query("peases",2) 
 print candidates
+
+for candidate in candidates:
+    candidate_edit_list = []
+    candidate_edit_list = levenshtein_edits(candidate,"peases")
+    print candidate_edit_list
 end = time.time()
 print end-start
 print load_finish-load_time
