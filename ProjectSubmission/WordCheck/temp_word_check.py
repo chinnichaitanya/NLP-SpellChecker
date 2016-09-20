b22b582c27a720e89dc3e91b4f09aab643d22611
chinnichaitanya <chchaitanya95@gmail.com>
2016-09-20 23:34:00 +0530

    fix bugs

--- ProjectSubmission/WordCheck/temp_word_check.py
+++ ProjectSubmission/WordCheck/temp_word_check.py
@@ -58,7 +58,7 @@ timeBayesianProbArray = time.time() - t
 t = time.time()
 # totalProbabilityArray = [pow(pow(0.6*p, 2)+pow(0.4*b, 2), 0.5) for p, b in zip(phoneticProbabilityArray, bayesianProbabilityArray)]
 # totalProbabilityArray = [0.6*p+0.4*b for p, b in zip(phoneticProbabilityArray, bayesianProbabilityArray)]
-totalProbabilityArray = [p*b*p for p, b in zip(phoneticProbabilityArray, bayesianProbabilityArray)]
+totalProbabilityArray = [p*b for p, b in zip(phoneticProbabilityArray, bayesianProbabilityArray)]
 # totalProbabilityArray = [1*b for b in phoneticProbabilityArray]
 tempSum = sum(totalProbabilityArray)
 for i in range(0, len(collectionSet)):
@@ -78,7 +78,7 @@ timeSortTotalDict = time.time() - t
 print('\nBelow are the suggestions for the word: ' + incorrectWord + '\n')
 print('Word\t\tProbability\t Rank')
 print('~~~~\t\t~~~~~~~~~~~\t ~~~~')
-for i in range(0, min(10, len(collectionSet)-1)):
+for i in range(0, min(10, len(collectionSet))):
 	print(str(totalDict[i][0]) + '\t\t' + str(round(100*totalDict[i][1], 2)) + '%\t\t Rank #' + str(i+1))
 
 print('\n\n##### Times (in sec) ####')
